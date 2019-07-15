e025d14a9426e7514ddedc91a4dd19a492fcdb9b
Kevin Suen <kevinsuen11@gmail.com>
2019-07-15 16:18:56 -0400

    Added bootstrap nav visuals

--- src/NavigationTabs.js
+++ src/NavigationTabs.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav'
 
 function NavigationTabs() {
   return (
-    <p>Navigation tabs here</p>
+    <div className="center-navbar">
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="mx-auto" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <NavDropdown className="mx-auto" title="Length" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Meter [m] to Mile [mi]</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Millimeter [mm] to Inch [in]</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Inch [in] to Feet [ft]</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown className="mx-auto" title="Mass" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Kilogram [kg] to Pound [lb]</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown className="mx-auto" title="Force" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Newton [N] to Pound Force [lbf]</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Kilonewton [kN] to Pound Force [lbf]</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   )
 }
 
