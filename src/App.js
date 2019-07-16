eb55735af716ab0b298bacd6c470baa00e01a60c
Kevin Suen <kevinsuen11@gmail.com>
2019-07-16 13:44:44 -0400

    added container row cols css

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React from 'react';
 import Header from './Header'
 import NavigationTabs from './NavigationTabs'
 import InputConversion from './InputConversion'
+import {Container, Row, Col, Nav} from 'react-bootstrap'
 
 function App() {
   return (
-    <div>
-      <Header />
-      <NavigationTabs />
-      <br />
-      <InputConversion />
-    </div>
+    <Container fluid>
+      <Row style={{textAlign:"center"}}>
+        <Col>
+          <Header />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <NavigationTabs />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <InputConversion />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
