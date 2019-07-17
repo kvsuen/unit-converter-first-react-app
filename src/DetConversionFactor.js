8750fc3f4040136f2f612ead9c4b962c1da82380
Kevin Suen <kevinsuen11@gmail.com>
2019-07-17 14:55:46 -0400

    Slight update for testing unit swap

--- src/DetConversionFactor.js
+++ src/DetConversionFactor.js
@@ -4,6 +4,8 @@ export default function DetConversionFactor(unitOne, unitTwo) {
 
   if (unitOne.value === "meter [m]" && unitTwo.value === "mile [mi]") {
     factor = 1 / 1609.344
+  } else if (unitOne.value === "mile [mi]" && unitTwo.value === "meter [m]") {
+    factor = 1609.344
   } else if (unitOne.value === "meter [m]" && unitTwo.value === "meter [m]") {
     factor = 1
   }
