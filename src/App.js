3b63fd672698df720d777675749f56d3c2297993
Kevin Suen <kevinsuen11@gmail.com>
2019-07-16 21:57:23 -0400

    added handleClick

--- src/App.js
+++ src/App.js
@@ -7,18 +7,28 @@ import {Container, Row, Col} from 'react-bootstrap'
 class App extends React.Component {
   state = {
     inputUnit: null,
-    outputUnit: null,
+    outputUnit: null
   };
 
   handleChange = (selectedOption, event) => {
     const { name } = event
-    console.log(event)
     this.setState({ [name]: selectedOption });
   };
 
   handleClick = (event) => {
-    const {name, value} = event.target
-    this.setState({ [name]: value })
+    const {name} = event.target
+    const firstUnit = name.slice(0, name.search("]") + 1)
+    const secondUnit = name.slice(name.search("]") + 2)
+    const firstUnitObject = {
+      value: firstUnit,
+      label: firstUnit.charAt(0).toUpperCase() + firstUnit.slice(1)
+    }
+    const secondUnitObject = {
+      value: secondUnit,
+      label: secondUnit.charAt(0).toUpperCase() + secondUnit.slice(1)
+    }
+    this.setState({ inputUnit: firstUnitObject})
+    this.setState({ outputUnit: secondUnitObject})
   }  
 
   render () {
